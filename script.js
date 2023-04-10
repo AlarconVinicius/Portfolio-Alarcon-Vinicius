7b197979cf86b4b286cab37b6bbe690bd50c4eff
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-10 09:34:32 -0300

    Portfolio

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-let urlGit = "https://raw.githubusercontent.com/AlarconVinicius/Portfolio-Alarcon-Vinicius/main/arq_json/projetos.json";
-
-let http = new XMLHttpRequest();
-http.open('get', urlGit, true);
-http.send();
-http.onload = function(){
-    if(this.readyState = 4 && this.status == 200){
-        let projects = JSON.parse(this.responseText);
-        console.log(projects);
-        let output = "";
-        for(let project of projects){
-
-            output += `
-                <div class="card">
-                    <div class="box">
-                        <div class="text">${project.titulo}</div>
-                        <p>${project.descricao}</p>
-                        <a href="${project.link}" target="_blank" class="btn-outline-green">Ver Mais</a>
-                    </div>
-                </div>
-            `;
-        }
-        document.querySelector("#projects-card").innerHTML = output;
-    }
-}
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
@@ -58,26 +33,52 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-    $('.carousel').owlCarousel({
-        margin:20,
-        loop:true,
-        autoplayTimeOut:2000,
-        autoplayHoverPauser:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:3,
-                nav:false
+    let urlGit = "https://raw.githubusercontent.com/AlarconVinicius/Portfolio-Alarcon-Vinicius/main/arq_json/projetos.json";
+
+    let http = new XMLHttpRequest();
+    http.open('get', urlGit, true);
+    http.send();
+    http.onload = function(){
+        if(this.readyState = 4 && this.status == 200){
+            let projects = JSON.parse(this.responseText);
+            console.log(projects);
+            let output = "";
+            for(let project of projects){
+
+                output += `
+                    <div class="card">
+                        <div class="box">
+                            <div class="text">${project.titulo}</div>
+                            <p>${project.descricao}</p>
+                            <a href="${project.link}" target="_blank" class="btn-outline-green">Ver Mais</a>
+                        </div>
+                    </div>
+                `;
             }
+            document.querySelector("#projects-card").innerHTML = output;
         }
-    });
+        $('.carousel').owlCarousel({
+            margin:20,
+            loop:true,
+            autoplayTimeOut:2000,
+            autoplayHoverPauser:true,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:false
+                },
+                600:{
+                    items:2,
+                    nav:false
+                },
+                1000:{
+                    items:3,
+                    nav:false
+                }
+            }
+        });
+    }
+    
 });
 
 // fetch("projetos.json")
